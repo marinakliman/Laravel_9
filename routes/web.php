d22435eeb5efbb46d7d8cbe3abc0fbe31e581c7c
marinakliman <152785926+marinakliman@users.noreply.github.com>
2025-05-21 20:27:13 +1000

    .

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Events\NewsHidden;
+use App\Models\News;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/news/create-test', function () {
+    $news = new News;
+    $news->title = 'Test news title';
+    $news->body = 'Test news body';
+
+    $news->save();
+    return $news;
+});
+
+Route::get('/news/{id}/hide', function ($id) {
+    $news = News::findOrFail($id);
+    $news->hidden = true;
+    $news->save();
+
+    NewsHidden::dispatch($news);
+    return 'News hidden';
+});
